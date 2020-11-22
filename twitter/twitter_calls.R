67a02424568d6259c243177f04b7b7695713ea1f
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-11-22 23:23:52 +0000

    refreshed twitter calls

--- twitter/twitter_calls.R
+++ twitter/twitter_calls.R
@@ -32,22 +32,44 @@ twts_climateaction %>%
 
 
 
-twts_food <- search_tweets("#food", n = 5000, include_rts = TRUE)
+twts_food <- search_tweets("#food", n = 15000, include_rts = TRUE)
 
 twts_food %>% 
     write_as_csv(file_name = here(paste0("raw_data/twts_food", "_", format(Sys.Date(), "%d%m%Y"), ".csv")), prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
 
-twts_lifestyle<- search_tweets("#food", n = 5000, include_rts = TRUE)
+twts_lifestyle<- search_tweets("#lifestyle", n = 15000, include_rts = TRUE)
 
 twts_lifestyle %>% 
     write_as_csv(file_name = here(paste0("raw_data/twts_lifestyle", "_", format(Sys.Date(), "%d%m%Y"), ".csv")), prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
 
-twts_home<- search_tweets("#food", n = 5000, include_rts = TRUE)
+twts_home<- search_tweets("#home", n = 15000, include_rts = TRUE)
 
 twts_home %>% 
     write_as_csv(file_name = here(paste0("raw_data/twts_home", "_", format(Sys.Date(), "%d%m%Y"), ".csv")), prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
 
-twts_travel<- search_tweets("#food", n = 5000, include_rts = TRUE)
+twts_travel<- search_tweets("#travel", n = 15000, include_rts = TRUE)
 
 twts_travel %>% 
     write_as_csv(file_name = here(paste0("raw_data/twts_travel", "_", format(Sys.Date(), "%d%m%Y"), ".csv")), prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+
+
+# Extract tweets on "#TedXGlasgow" and include retweets    
+twts_COP26Action <- search_tweets("#COP26Action", 
+                           n = 18000, 
+                           include_rts = TRUE, 
+                           lang = "en")
+
+
+twts_COP26Action %>% 
+    write_as_csv(file_name = here(paste0("raw_data/twts_COP26Action", "_", format(Sys.Date(), "%d%m%Y"), ".csv")), prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+
+
+# Extract tweets on "#TedXGlasgow" and include retweets    
+twts_COP26<- search_tweets("@COP26", 
+                                  n = 18000, 
+                                  include_rts = TRUE, 
+                                  lang = "en")
+
+
+twts_COP26 %>% 
+    write_as_csv(file_name = here(paste0("raw_data/twts_COP26", "_", format(Sys.Date(), "%d%m%Y"), ".csv")), prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
